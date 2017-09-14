6f45270575e1ab3d2cbb8f5f3e5444a5bed6726b
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-13 23:10:01 -0300

    Melhorando nosso código, aplicando arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -16,10 +16,8 @@ class NegociacaoController{
         let data = new Date(...
             this._inputData.value
             .split('')
-            .map(function(item, indice) {
-                return item - indice % 2
-            });
+            .map((item, indice) => item - indice % 2 )
         );
-        console.log(data);
+        
     }
 }
