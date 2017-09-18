ad9f628605088a80db2005f2ad5e81c645d2c36f
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-17 23:58:57 -0300

    aplicando expressão regular para pegar a data correta aaaa-mm-dd

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -9,6 +9,10 @@ class DataHelper{
     }
 
     static textoParaData(texto) {
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
         return new Date(...texto.split('').map((item, indice) => item - indice % 2 ) );
     }
 
