a8f4553f87a3de4ab04b57ef87bc17431f9a4547
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-20 03:01:29 -0300

    Correções em DateHelper

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,7 +5,7 @@ class DateHelper{
     }
 
     static dataParaTexto(data) {
-        return `${ data.getDate()} / ${data.getMonth()+1} / ${data.getFullYear()} }`;
+        return `${ data.getDate()} / ${data.getMonth()+1} / ${data.getFullYear()}`;
     }
 
     static textoParaData(texto) {
@@ -13,7 +13,7 @@ class DateHelper{
         if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
 
-        return new Date(...texto.split('').map((item, indice) => item - indice % 2 ) );
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2 ) );
     }
 
 }
