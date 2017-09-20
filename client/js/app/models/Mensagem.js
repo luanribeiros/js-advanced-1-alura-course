8dfe193396d4b48eb7596d3edee51984520bdc5d
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-20 00:47:28 -0300

    criando alguns métodos em nossa classe mensagem

--- client/js/app/models/Mensagem.js
+++ client/js/app/models/Mensagem.js
@@ -1,3 +1,15 @@
 class Mensagem{
-    
+
+    constructor() {
+        
+        this._texto;
+    }
+
+    get texto() {
+        return this._texto;
+    }
+
+    set texto() {
+        this._texto = texto;
+    }
 }
