98666fc55a6f5abe48c6e8297977e30ffad9f485
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-20 05:09:25 -0300

    Ajustes para notificação de 'mensagem adicionada com sucesso'

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -22,8 +22,10 @@ class NegociacaoController{
         event.preventDefault();
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);            
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._negociacoesView.update(this._listaNegociacoes);    
+        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();
     }
 
