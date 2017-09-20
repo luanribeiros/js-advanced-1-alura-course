8825456b464912c4f5072cb8d37cf3e20ea37c6c
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-20 00:33:05 -0300

    construindo o tfoot

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -18,19 +18,27 @@ class NegociacoesView {
                 </thead>
                 
                 <tbody>
-                    ${ model.negociacoes.map( n => {
-                        return ` 
+                    ${ model.negociacoes.map( n => ` 
+                           
                             <tr>
                                 <td> ${DateHelper.dataParaTexto(n.data)} </td>
                                 <td> ${n.quantidade} </td>
                                 <td> ${n.valor} </td>
                                 <td> ${n.volume} </td>
                             </tr>
-                        `;
-                    }).join('')}
+                        
+                        `).join('')}
                 </tbody>
                 
                 <tfoot>
+                    <td colspan="3"></td>
+                    <td> ${
+                        function() {
+                            let total = 0;
+                            model.negociacoes.forEach( n => total += n.volume);
+                            return total;
+                        }
+                    }</td>
                 </tfoot>
             </table>
         `;
